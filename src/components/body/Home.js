234b9627458ae193d58a7d43b92eb0bda4a580e8
hasibkyau <hasibkyau.cse@gmail.com>
2022-02-12 21:51:23 +0600

    added font-awesome

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -1,11 +1,12 @@
 import React, {Component} from "react";
+import Loading from "./Loading";
 
 class Home extends Component{
     render(){
         document.title = "Home";
         return(
             <div>
-
+                <Loading/>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'font-awesome/css/font-awesome.css'
 ReactDOM.render(<App />,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
