c2775f87165070e8e512e56d4dc885e0419a1a68
hasibkyau <hasibkyau.cse@gmail.com>
2022-02-15 23:01:11 +0600

    added new branch

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import MainComponent from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+import myStore from './redux/store';
+import { Provider } from 'react-redux';
+
+function App() {
+  //console.log("App.js", myStore.getState());
+  return (
+    <div className="App">
+      <Provider store={myStore}>
+        <BrowserRouter>
+            <MainComponent />
+        </BrowserRouter>
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navigation from "./Navigation";
+const Header = () =>{
+    return(
+        <div>
+            <Navigation/>
+        </div>
+    );
+}
+
+export default Header;
