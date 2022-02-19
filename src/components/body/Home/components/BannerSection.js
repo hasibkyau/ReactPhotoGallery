47899458c754a19ed4610db92c701e53fd2bc4de
hasibkyau <hasibkyau.cse@gmail.com>
2022-02-19 16:34:42 +0600

    solved banner alignment issue

--- src/components/body/Home/components/BannerSection.js
+++ src/components/body/Home/components/BannerSection.js
@@ -6,7 +6,8 @@ const BannerSection = (props) => {
         <div>
             <div className="banner" style={{backgroundImage: "linear-gradient(to right, rgba(2, 2, 2, 0.2), rgba(0, 0, 0, 0.2)),url("+props.bgImg+")"}}>
                 <div className="container">
-                    <div className="row" id="hello">
+           
+
                         <div className="banner-text">
                             <div className="companyName">
                                 <span style={{ color: "white"}}>{props.companyName.fname}</span>
@@ -18,7 +19,6 @@ const BannerSection = (props) => {
                             {/* <Button color="danger" size="sm">Sign Up for Free</Button> */}
                             <button type="button" className="btn btn-danger" style = {{ backgroundColor: props.button.backgroundColor, width: props.button.size}}>Sign Up for Free</button>
                         </div>
-                    </div>
                 </div>
             </div>
         </div>
