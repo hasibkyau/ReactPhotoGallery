1fdedc83830fb609f73420b6832e7b48be55e5ac
hasibkyau <hasibkyau.cse@gmail.com>
2022-03-03 18:43:39 +0600

    messed up

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./header/Header";
 import Body from "./body/Body";
-import Footer from "./body/Home/components/Footer";
 
 const MainComponent = () =>{
     return(
