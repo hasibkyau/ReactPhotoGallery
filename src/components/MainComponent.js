5388486d3010c21b2ae04a5dadb44d81a28dffae
hasibkyau <hasibkyau.cse@gmail.com>
2022-03-03 19:54:05 +0600

    added routes and contact

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Header from "./header/Header";
 import Body from "./body/Body";
+import Contact from "./body/Contact";
+import { Routes, Route } from "react-router";
 
 const MainComponent = () =>{
     return(
         <div>
             <Header/>
-            <Body/>
+            <Routes>
+                <Route path="/" element={<Body/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+            </Routes>
         </div>
     );
 }
