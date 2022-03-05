d44d85e96e420eab0b3072fa482b1ac4e6b027a9
hasibkyau <hasibkyau.cse@gmail.com>
2022-03-05 09:49:39 +0600

    Route not working

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,66 +1,77 @@
 import React, { Component } from "react";
 import Header from "./header/Header";
 import Body from "./body/Body";
+import Home from "./body/Home/Home";
 import Contact from "./body/Contact";
 import Feedback from "./feedback/Feedback";
 import Auth from "./Auth/Auth";
-import { Redirect, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
-
+import { Redirect, Route, Switch } from "react-router-dom";
 import { connect } from 'react-redux';
 import { authCheck } from '../redux/authActionCreators';
 import Logout from "./Auth/Logout";
 
-const mapStateToProps = state => {
-    return {
-        token: state.token,
-        userId: state.userId,
-    }
-}
-const mapDispatchToProps = dispatch => {
-    return {
-        authCheck: () => dispatch(authCheck()),
-    }
-}
+// const mapStateToProps = state => {
+//     return {
+//         token: state.token,
+//         userId: state.userId,
+//     }
+// }
+// const mapDispatchToProps = dispatch => {
+//     return {
+//         authCheck: () => dispatch(authCheck()),
+//     }
+// }
 
 
 class MainComponent extends Component {
-    componentDidMount() {
-        this.props.authCheck();
-        console.log(this.props);
-    }
+    // componentDidMount() {
+    //     this.props.authCheck();
+    //     console.log(this.props);
+    // }
     render() {
-        let routes = null;
-        if (this.props.token === null) {
-            routes = (
-                <Switch>
-                    <Route path="/login" component={Auth} />
-                    <Redirect to="/login" />
-                </Switch>
-            )
-        } else {
-            routes = (
-                <div>
-                    <Header />
-                    <Switch>
-                        <Route path="/logout" Component={Logout} />
-                        <Route path="/" Component={Body} />
-                        <Route path="/contact" Component={Contact} />
-                        <Route path="/feedback" Component={Feedback} />
-                        <Redirect to="/" />
-                    </Switch>
-                </div>
-            )
-        }
+        // let routes = null;
+        // if (this.props.token === null) {
+        //     routes = (
+        //         <Switch>
+        //             <Route path="/login" component={Auth} />
+        //             <Redirect to="/login" />
+        //         </Switch>
+        //     )
+        // } else {
+        //     routes = (
+        //         <div>
+        //             <Header />
+        //             <Switch>
+        //                 <Route path="/logout" Component={Logout} />
+        //                 <Route path="/" Component={Body} />
+        //                 <Route path="/contact" Component={Contact} />
+        //                 <Route path="/feedback" Component={Feedback} />
+        //                 <Redirect to="/" />
+        //             </Switch>
+        //         </div>
+        //     )
+        // }
+
         return (
             <div>
                 <div className="container">
                 <Header />
+                {/* <Home/> */}
                     {/* {routes} */}
-                    
+                    <Switch>
+                        
+                        <Route path="/logout" Component={Logout} />
+                        <Route path="/" Component={Home} />
+                        <Route path="/contact" Component={Contact} />
+                        <Route path="/feedback" Component={Feedback} />
+                        {/* <Redirect to="/" /> */}
+                    </Switch>     
                 </div>
             </div>
         )
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(MainComponent);
+export default MainComponent;
+
+//export default connect(mapStateToProps, mapDispatchToProps)(MainComponent);
--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,18 +1,12 @@
 import React from "react";
 import Contact from "./Contact";
-import { Route, Routes } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 import Home from "./Home/Home";
-import Photos from "./Photos";
-import { Switch } from "react-router-dom/cjs/react-router-dom.min";
 
 const Body = () => {
     return (
         <div>
-            <Switch>
-                <Route path="/" Component={Home} />
-                <Route path="/menu" Component={Photos} />
-                <Route path="/contact" Component={Contact} />
-            </Switch>
+            <Home/>
         </div>
     );
 }
